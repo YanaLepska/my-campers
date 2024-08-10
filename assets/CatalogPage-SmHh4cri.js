a7f5cea0dbfc5b10d4099be5253461593a7014aa
YanaLepska <lepskayana1624@gmail.com>
2024-08-11 02:43:06 +0300

    Updates

---
+++ assets/CatalogPage-SmHh4cri.js
@@ -0,0 +1,2 @@
+import{j as a}from"./index-DDWWsqS8.js";import{C as t}from"./CamperList-DYFG3HGI.js";const s="_catalogPage_10fdf_1",o={catalogPage:s},g=()=>a.jsx("div",{className:o.catalogPage,children:a.jsx(t,{})});export{g as default};
+//# sourceMappingURL=CatalogPage-SmHh4cri.js.map
