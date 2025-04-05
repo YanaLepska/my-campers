f453809946430a8ccc7a500a54e9734155f6106c
YanaLepska <lepskayana1624@gmail.com>
2025-04-05 23:11:38 +0300

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -7,7 +7,7 @@ const Loader = () => {
   visible={true}
   height="80"
   width="80"
-  color="rgb(248, 179, 119)"
+  color="rgba(240, 200, 200, 0.75)"
   ariaLabel="triangle-loading"
   wrapperStyle={{}}
   wrapperClass=""
